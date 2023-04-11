e01fef663b92eb94b4dee8b2902af9bb1aea02c4
karlos1000 <karlos.0@live.com.mx>
2023-04-11 16:46:24 -0600

    Carga inicial

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # ejercicio004
+Create a Vue component that allows a user to enter their first and last name on a web page.
+When the user clicks a submit button, the component should display a custom welcome message that includes the user's first and last name.
+You must make use of Vue's v-model and v-on directives to handle user input and display the welcome message.
 
 ## Project setup
 ```
---
+++ src/components/User.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-show="option_user">
+        <div>
+            <label>Nombre:</label>
+            <input type="text" v-model="user">
+        </div>
+        <div>
+            <label>Apellido:</label>
+            <input type="text" v-model="lastname">
+        </div>
+        <div>
+            <button v-on:click="send">Enviar</button>
+        </div>
+    </div>
+
+    <div v-show="option_b">
+        Bienvenido
+        <div>Nombre: {{ user }}</div>
+        <div>Apellido: {{ lastname }}</div>
+    </div>
+</template>
+
+<script>
+import {ref} from "vue";
+export default {
+    name: 'User',
+    props: {
+    },
+    setup() {
+        let user = ref("");
+        let lastname = ref("");
+        let option_user = ref(true);
+        let option_b = ref(false);
+
+        const send = () => {
+            option_user.value = false;
+            option_b.value = true;
+            console.log("send", user.value, lastname.value);
+        }
+
+        return {
+            user,
+            lastname,
+            option_user,
+            option_b,
+            send,
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
